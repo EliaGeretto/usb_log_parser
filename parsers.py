b955ac668c8fbcb8335c282112a210d12f6c6e82
Elia Geretto <elia.f.geretto@gmail.com>
2017-05-31 15:28:54 +0200

    Push the parsing of the datetime in the parser subclass

--- parsers.py
+++ parsers.py
@@ -1,8 +1,16 @@
+from systemd import journal
+from collections import namedtuple
+from datetime import datetime
 import abc
 import re
+
 from usb_info import UsbInfo
 
 
+LogEntry = namedtuple('LogEntry', ['timestamp', 'message'])
+
+
+# XXX: Needs review, given the new internal API
 class Parser(metaclass=abc.ABCMeta):
     def __init__(self, src):
         self.__usb_data = self.retrieve_data(src)
@@ -55,20 +63,26 @@ class SyslogParser(Parser):
     def __init__(self, src):
         # The method retrieve_data will be called by the constructor of the
         # parent class, thus this initialization goes before the call to it.
-        self.__data_pattern = re.compile(
-                ': New USB device found,|: USB disconnect,|: Product: |'
-                ': Manufacturer: |: SerialNumber: ')
+        self.__kern_pattern = re.compile('kernel: \[\s*[0-9]+\.[0-9]+\] ')
 
         super().__init__(src)
 
     def retrieve_data(self, src):
-        usb_data = []
-        with open(src) as origin:
-            for line in origin:
-                if re.search(self.__data_pattern, line):
-                    usb_data.append(line)
+        usb_entries = []
+        with open(src) as syslog:
+            for line in syslog:
+                # Kernel log message
+                if re.search(self.__kern_pattern, line):
+                    message = re.split(self.__kern_pattern, line)[1]
+                    # USB subsystem message
+                    if message.startswith('usb') or message.startswith('hub'):
+                        date_string = line[:15]
+                        date = datetime.strptime(
+                                date_string, '%b %d %H:%M:%S')
+                        log_entry = LogEntry(date, message)
+                        usb_entries.append(log_entry)
 
-        return usb_data
+        return usb_entries
 
 
 class JournaldParser(Parser):
@@ -76,5 +90,16 @@ class JournaldParser(Parser):
         super().__init__(src)
 
     def retrieve_data(self, src):
-        # TODO: Extract data from journald log
-        pass
+        reader = journal.Reader(path=src)
+        reader.add_match(_KERNEL_SUBSYSTEM='usb')
+
+        usb_entries = []
+        for journal_entry in reader:
+            # The __REALTIME_TIMESTAMP is already a datetime object
+            log_entry = LogEntry(
+                    journal_entry['__REALTIME_TIMESTAMP'],
+                    journal_entry['MESSAGE'])
+            usb_entries.append(log_entry)
+        reader.close()
+
+        return usb_entries
--- usb_info.py
+++ usb_info.py
@@ -2,11 +2,10 @@ import unittest
 from datetime import datetime
 
 
-DATETIME_FMT_IN = '%b %d %H:%M:%S'
-DATETIME_FMT_OUT = '%B %d %H:%M:%S'
-
-
 class UsbInfo:
+
+    DATETIME_FMT = '%B %d %H:%M:%S'
+
     def __init__(self):
         self.product = None
         self.manufact = None
@@ -16,21 +15,21 @@ class UsbInfo:
 
     @property
     def time_in(self):
-        return self.__time_in.strftime(DATETIME_FMT_OUT) \
+        return self.__time_in.strftime(UsbInfo.DATETIME_FMT) \
                 if self.__time_in is not None else None
 
     @time_in.setter
     def time_in(self, value):
-        self.__time_in = datetime.strptime(value, DATETIME_FMT_IN)
+        self.__time_in = value
 
     @property
     def time_out(self):
-        return self.__time_out.strftime(DATETIME_FMT_OUT) \
+        return self.__time_out.strftime(UsbInfo.DATETIME_FMT) \
                 if self.__time_out is not None else None
 
     @time_out.setter
     def time_out(self, value):
-        self.__time_out = datetime.strptime(value, DATETIME_FMT_IN)
+        self.__time_out = value
 
     def __str__(self):
         return ('Product: {} ; Manufacturer: {} ; Serial number: {} ; '
@@ -40,6 +39,9 @@ class UsbInfo:
 
 
 class UsbInfoTest(unittest.TestCase):
+
+    TEST_FMT = '%b %d %H:%M:%S'
+
     def setUp(self):
         self.usb_info = UsbInfo()
 
@@ -53,8 +55,10 @@ class UsbInfoTest(unittest.TestCase):
         self.usb_info.product = 'Hello'
         self.usb_info.manufact = 'World'
         self.usb_info.serial = '123456'
-        self.usb_info.time_in = 'May  5 05:05:05'
-        self.usb_info.time_out = 'Oct 10 10:10:10'
+        self.usb_info.time_in = datetime.strptime(
+                'May  5 05:05:05', UsbInfoTest.TEST_FMT)
+        self.usb_info.time_out = datetime.strptime(
+                'Oct 10 10:10:10', UsbInfoTest.TEST_FMT)
 
         self.assertEqual(str(self.usb_info),
                          ('Product: Hello ; Manufacturer: World ; '
