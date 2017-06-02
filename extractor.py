894e453e431ec4af31a88bfe2bd0e02f2d3f346b
Elia Geretto <elia.f.geretto@gmail.com>
2017-06-02 13:05:04 +0200

    Change log searching heuristics

--- extractor.py
+++ extractor.py
@@ -9,46 +9,42 @@ from parsers import JournaldParser, SyslogParser
 class Extractor:
     def __init__(self, image_files):
         self.__image_files = image_files
+        self.__results = []
 
-    def __mount_volume(self):
+    def __prepare_parser(self):
         logging.info('Parsing image from files {}'.format(self.__image_files))
         self.__image_parser = ImageParser(self.__image_files)
 
+    def __parse_logs(self):
         for vol in self.__image_parser.init():
-            if vol.size > 1024 * 1024:
-                logging.info('Found suitable volume {}'.format(vol.index))
-                self.__volume = vol
-                break
+            vol.init_volume()
+            vol_mountpoint = Path(vol.mountpoint)
 
-        self.__volume.init_volume()
+            journald_log_dir = vol_mountpoint / 'var' / 'log' / 'journal'
+            syslog_file = vol_mountpoint / 'var' / 'log' / 'syslog'
+            if journald_log_dir.exists() and journald_log_dir.is_dir():
+                logging.info('Found systemd journal log in volume {}'.format(
+                        vol.index))
+                info_parser = JournaldParser(str(journald_log_dir.resolve()))
+                self.__results += info_parser.results
+            elif syslog_file.exists() and syslog_file.is_file():
+                logging.info('Found syslog log in volume {}'.format(
+                        vol.index))
+                info_parser = SyslogParser(str(syslog_file.resolve()))
+                self.__results += info_parser.results
 
-    def __unmount_volume(self):
+    def __unmount_volumes(self):
         self.__image_parser.clean()
 
-    def __init_log_parser(self):
-        vol_mountpoint = Path(self.__volume.mountpoint)
-
-        journald_log_dir = vol_mountpoint / 'var' / 'log' / 'journal'
-        syslog_file = vol_mountpoint / 'var' / 'log' / 'syslog'
-        if journald_log_dir.exists() and journald_log_dir.is_dir():
-            logging.info('Found systemd journal log in volume {}'.format(
-                    self.__volume.index))
-            self.__info_parser = JournaldParser(
-                    str(journald_log_dir.resolve()))
-        elif syslog_file.exists() and syslog_file.is_file():
-            logging.info('Found syslog log in volume {}'.format(
-                    self.__volume.index))
-            self.__info_parser = SyslogParser(
-                    str(syslog_file.resolve()))
-        else:
-            logging.error('No log found')
-
     def get_usb_info(self):
-        self.__mount_volume()
-        self.__init_log_parser()
+        self.__prepare_parser()
+        self.__parse_logs()
 
         # Otherwise it is too fast
         sleep(0.1)
-        self.__unmount_volume()
+        self.__unmount_volumes()
 
-        return self.__info_parser.results
+        if self.__results == []:
+            logging.error('No log was found in any volume')
+
+        return self.__results
